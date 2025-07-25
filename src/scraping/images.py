96b44cd6c4800d1366cd8b93627811998e9e9225
Mark <mark.oconnor14@gmail.com>
2025-07-25 11:14:34 -0700

    make script to read image links from file and store in db

--- src/scraping/images.py
+++ src/scraping/images.py
@@ -1,15 +1,11 @@
-# step 1 just get the images
-# save them to a text file with pokemon id - name - image url (or json)
-# then you can check that out and see if anything is missing or wrong
-# then you can use that to write to the db
-# I need to update the DB, so I'll have to figure out some kind of migrations system
-# or use SQL Alchemy
-# Might be fun to use ALCHEMY with raw SQL or Postgres
-
-from pprint import pprint
+import sqlite3
 import requests
+import re
 
 from bs4 import BeautifulSoup, SoupStrainer
+
+from src.queries import list_pokemon
+from src.types import Pokemon
 
 URL = "https://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_by_National_Pok%C3%A9dex_number"
 
@@ -28,16 +24,68 @@ def scrape_pokemon_thumbnails():
  
     print(f"job complete")
 
-# def WIP_scrape_links_to_detail_pages():
-#     r = requests.get(URL)
-#     text = r.text
-#     only_tables = SoupStrainer('table')
 
-#     soup = BeautifulSoup(text, "html.parser", parse_only=only_tables)
-#     first_table = soup.find('table', class_='roundy')
+def store_thumbnails_links():
+    print(f"Running script to store thumbnail links")
+    pokemon_rows = list_pokemon()
+    reg_filter = r"-[A-Za-z_]+"
+    pokemon_dict = {}
 
-#     needed_links = first_table.select('a[title*=Pokémon]')
+    # filter out images of pokemon alternatives
+    thumbnails_to_keep = []
+    with open("./images.txt", "r") as thumbnail_file:
+        thumbnail_links = thumbnail_file.read().splitlines()
+        for link in thumbnail_links:
+            if re.search(reg_filter, link):
+                print(f'found one to remove: {link}')
+                # del thumbnail_links[index]
+            else:
+                print(f'found one to keep: {link}')
+                thumbnails_to_keep.append(link)
 
-#     pprint(needed_links)
-#     print(f"job complete")
+    # match pokemon rows with thumbnails
+    # create Pokemon objects for each to make it easier to work with
+    for row in pokemon_rows:
+        if row[0] < 10:
+            number_to_find = f"000{row[0]}"
+            if number_to_find in thumbnails_to_keep[row[0]-1]:
+                pokemon_dict[f"000{row[0]}"] = Pokemon(
+                    name=row[1],
+                    pokemon_id=row[2],
+                    pokemon_order=row[3],
+                    thumbnail=thumbnails_to_keep[row[0]-1]
+                )
+        elif row[0] >= 10 and row[0] < 100:
+            number_to_find = f"00{row[0]}"
+            if number_to_find in thumbnails_to_keep[row[0]-1]:
+                pokemon_dict[f"00{row[0]}"] = Pokemon(
+                    name=row[1],
+                    pokemon_id=row[2],
+                    pokemon_order=row[3],
+                    thumbnail=thumbnails_to_keep[row[0]-1]
+                )
+        else:
+            number_to_find = f"0{row[0]}"
+            if number_to_find in thumbnails_to_keep[row[0]-1]:
+                pokemon_dict[f"0{row[0]}"] = Pokemon(
+                    name=row[1],
+                    pokemon_id=row[2],
+                    pokemon_order=row[3],
+                    thumbnail=thumbnails_to_keep[row[0]-1]
+                )
 
+    # update rows in the database
+    update_params = [(value.thumbnail, value.pokemon_id) for value in pokemon_dict.values()]
+    try:
+        with sqlite3.connect("pokemon.db") as connection:
+            cursor = connection.cursor()
+
+            cursor.executemany(
+                '''UPDATE pokemon SET thumbnail = ? WHERE id = ?''',
+                update_params
+            )
+
+            connection.commit()
+
+    except Exception as e:
+        raise Exception(f"an error occured: {e}")
