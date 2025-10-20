755d442c1556ca637a1298215db75789b11b0abf
Mark <mark.oconnor14@gmail.com>
2025-10-20 09:42:00 +0800

    create seeder for halloween monsters

---
+++ seeders/setup_monster.py
@@ -0,0 +1,12 @@
+from models.monster import Monster
+
+
+monsters = ["vampire", "zombie", "mummy", "werewolf", "witch", "ghost", "skeleton", "jack-o-lantern", "scarecrow"]
+
+for monster in monsters:
+    Monster.create(name=monster)
+
+db_monsters = Monster.list()
+
+for monster in db_monsters:
+    print(monster)
