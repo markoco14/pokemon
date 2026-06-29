0f798ad8cb3793ea2edf904205056b3c576c20af
Mark O'Connor <mark.oconnor14@gmail.com>
2026-06-29 10:30:35 +0800

    update christmas endpoints for word table

--- src/controllers/christmas.py
+++ src/controllers/christmas.py
@@ -1,9 +1,11 @@
 import random
 import sqlite3
-from fastapi import Request
+from typing import Annotated
+from fastapi import Depends, Request
 
 from fastapi.responses import RedirectResponse
 
+from src.dependencies import get_db
 from src.templates import templates
 
 async def index(request: Request):
@@ -14,56 +16,53 @@ async def index(request: Request):
     )
 
 
-async def teach(request: Request):
-    with sqlite3.connect("esl.db") as conn:
-        conn.execute("PRAGMA journal_mode = WAL;")
-        conn.execute("PRAGMA foreign_keys = ON;")
-        conn.row_factory = sqlite3.Row
-
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM christmas;")
-        vocab_set = cursor.fetchall()
-
+async def teach(request: Request, conn: Annotated[sqlite3.Connection, Depends(get_db)]):
+    
+    rows = conn.execute(
+        """
+        SELECT w.word_id, w.word, w.large_img_path FROM word AS w
+        JOIN word_category AS wc ON wc.word_id = w.word_id
+        JOIN category AS c ON c.category_id = wc.category_id
+        WHERE c.name = 'christmas';
+        """
+        ).fetchall()
     
     return templates.TemplateResponse(
         request=request,
         name="christmas/teach.html",
-        context={"vocab_set": vocab_set}
+        context={"vocab_set": rows}
     )
 
 
-def get_random_word():
-    with sqlite3.connect("esl.db") as conn:
-        conn.execute("PRAGMA journal_mode = WAL;")
-        conn.execute("PRAGMA foreign_keys = ON;")
-        conn.row_factory = sqlite3.Row
+def get_random_word(conn: sqlite3.Connection):
 
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM christmas;")
-        vocab_set = cursor.fetchall()
+    rows = conn.execute(
+        """
+        SELECT w.word_id, w.word, w.large_img_path FROM word AS w
+        JOIN word_category AS wc ON wc.word_id = w.word_id
+        JOIN category AS c ON c.category_id = wc.category_id
+        WHERE c.name = 'christmas';
+        """
+        ).fetchall()
 
-    random_index = random.randrange(len(vocab_set))
+    random_index = random.randrange(len(rows))
 
-    return vocab_set[random_index]
+    return rows[random_index]
 
 
-async def see_and_say(request: Request):
+async def see_and_say(request: Request, conn: Annotated[sqlite3.Connection, Depends(get_db)]):
     if not request.query_params.get("word"):
-        word = get_random_word()
-        return RedirectResponse(status_code=303, url=f"/christmas/see-and-say?word={word['name']}")
+        word = get_random_word(conn=conn)
+        return RedirectResponse(status_code=303, url=f"/christmas/see-and-say?word={word['word']}")
     
-    with sqlite3.connect("esl.db") as conn:
-        conn.execute("PRAGMA journal_mode = WAL;")
-        conn.execute("PRAGMA foreign_keys = ON;")
-        conn.row_factory = sqlite3.Row
+    word = conn.execute(
+        "SELECT * FROM word WHERE word = :word;", 
+        {"word": request.query_params.get("word")}
+        ).fetchone()
 
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM christmas WHERE name = ?;", (request.query_params.get("word"),))
-        word = cursor.fetchone()
-
-    next_word = get_random_word()
-    while next_word["name"] == word["name"]:
-        next_word = get_random_word()
+    next_word = get_random_word(conn=conn)
+    while next_word["word"] == word["word"]:
+        next_word = get_random_word(conn=conn)
     
     return templates.TemplateResponse(
         request=request,
