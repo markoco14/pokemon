6020104e13bc1dff60e47297dd8c809179f74aff
Mark O'Connor <mark.oconnor14@gmail.com>
2026-07-19 13:00:32 +0800

    update game repository to serialize choice_ids

--- src/repositories/game_repository.py
+++ src/repositories/game_repository.py
@@ -1,4 +1,6 @@
+import json
 import sqlite3
+from typing import List
 
 
 def list_by_category(conn: sqlite3.Connection, category: str):
@@ -8,8 +10,27 @@ def list_by_category(conn: sqlite3.Connection, category: str):
         ).fetchall()
 
 
-def insert(conn: sqlite3.Connection, url_path: str, type: str, category: str, answer_id: int):
+def insert(
+        conn: sqlite3.Connection, 
+        url_path: str, 
+        type: str, 
+        category: str, 
+        answer_id: int, 
+        choice_ids: List[int]
+        ):
+    serialized_choice_ids = json.dumps(choice_ids)
     cursor = conn.cursor()
     cursor.execute(
-        "INSERT INTO game (url_path, type, category, answer_id) VALUES (:url_path, :type, :category, :answer_id)", 
-        {"url_path": url_path, "type": type, "category": category, "answer_id": answer_id})
+        """
+        INSERT INTO game (
+        url_path, type, category, answer_id, choice_ids
+        ) VALUES (
+        :url_path, :type, :category, :answer_id, :choice_ids
+        );""", 
+        {"url_path": url_path, 
+         "type": type, 
+         "category": category, 
+         "answer_id": answer_id, 
+         "choice_ids": serialized_choice_ids
+         })
+    conn.commit()
