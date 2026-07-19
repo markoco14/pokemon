c7bc927b914f0815fc63c87ac88cca6befe23f3a
Mark O'Connor <mark.oconnor14@gmail.com>
2026-07-19 22:15:07 +0800

    clean up

--- src/controllers/pokemon.py
+++ src/controllers/pokemon.py
@@ -8,15 +8,8 @@ from fastapi.responses import RedirectResponse
 
 from src.repositories import game_repository, word_repository
 from src.dependencies import get_db
-from src.types import Game
-from src.utils import get_s3_domain
-
-
-
 from src.templates import templates
-
-games = {}
-
+from src.utils import get_s3_domain
 
 
 async def index(
@@ -41,54 +34,6 @@ async def index(
 
 
 async def whos_that_pokemon_redirect(
-        request: Request,
-        conn: Annotated[sqlite3.Connection, Depends(get_db)]
-        ):
-    game_id = random.randint(1000, 10001)
-
-    # TODO: change this back to only select 4 pokemon
-    # requires some work because pokemon ids are lost
-    # need some kind of word_meta table or pokemon_meta table 
-    # to connect pokemon numbers to words
-    pokemon = word_repository.list_by_category(conn=conn, category="pokemon")
-    
-    chosen_pokemon = random.sample(population=pokemon, k=4)
-    
-    answer = chosen_pokemon[random.randint(0,3)]
-
-    games[game_id] = Game(
-        id=game_id,
-        answer=answer,
-        pokemons=chosen_pokemon,
-        guesses=[],
-        finished=False
-    )
-
-    return RedirectResponse(url=f"/whos-that-pokemon/{game_id}")
-
-
-async def whos_that_pokemon_game(request: Request, game_id: int):
-    if game_id not in games:
-        return RedirectResponse(url="/", status_code=303)  
-
-    return templates.TemplateResponse(
-        request=request, name="pokemon/whos-that-pokemon.html", context={"game": games[game_id]}
-    )
-
-def whos_that_pokemon_guess(request: Request, game_id: int, guess_pokemon_id: int):
-    if not games.get(game_id):
-        return RedirectResponse(url="/", status_code=303)
-
-    if guess_pokemon_id not in games[game_id].guesses:
-        games[game_id].guesses.append(guess_pokemon_id)
-
-    if guess_pokemon_id == games[game_id].answer["word_id"]:
-        games[game_id].finished = True
-    
-    return RedirectResponse(url=f"/whos-that-pokemon/{game_id}")
-
-
-async def whos_that_pokemon_redirect_v2(
         request: Request,
         conn: Annotated[sqlite3.Connection, Depends(get_db)]
         ):
@@ -117,8 +62,7 @@ async def whos_that_pokemon_redirect_v2(
     return RedirectResponse(url=f"/whos-that-pokemon/{url_path}")
 
 
-
-async def whos_that_pokemon_game_v2(
+async def whos_that_pokemon_game(
         request: Request, 
         conn: Annotated[sqlite3.Connection, Depends(get_db)],
         url_path: str):
@@ -154,7 +98,7 @@ async def whos_that_pokemon_game_v2(
     )
 
 
-def whos_that_pokemon_guess_v2(
+def whos_that_pokemon_guess(
         request: Request,
         conn: Annotated[sqlite3.Connection, Depends(get_db)],
         url_path: str, 
